20ae85a9a75a352408be8e88f18ebcf1ef35926d
Loren Ndung'u <ndunguloren96@gmail.com>
2025-08-30 11:35:55 +0300

    fix `NameError`, `JsonResponse` and add missing redis_data

--- properties/views.py
+++ properties/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 from .models import Property
+from .utils import get_all_properties # Add this import for Task 2
 
-@cache_page(60 * 15) # Cache the view for 15 minutes
 def property_list(request):
     """
-    View to list all properties, cached at the view level.
+    View to list all properties, returning JSON data.
     """
-    properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    properties = get_all_properties()
+    
+    # We now convert the queryset objects to a list of dictionaries to make them JSON-serializable.
+    data = [{
+        'title': p.title,
+        'description': p.description,
+        'price': str(p.price), # DecimalField needs to be converted to a string
+        'location': p.location,
+        'created_at': p.created_at.isoformat(), # datetime object needs to be converted
+    } for p in properties]
+    
+    return JsonResponse({'data': data}, safe=False)
